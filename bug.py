46d2d5ddf448a6aa20450651a9433a3842486d0a
gromova-daria <146058791+gromova-daria@users.noreply.github.com>
2023-10-21 18:51:47 +0300

    bugForm added

--- bug.py
+++ bug.py
@@ -3,12 +3,11 @@ import tkinter as tk
 
 
 root = tk.Tk()
-root.title("Добро пожаловать в КНТ-6")
+root.title("Ошибка")
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-sex='9'
-date='qawsedrftgyhujikolp'
+
 
 window_width = 500
 window_height = 500
@@ -16,37 +15,16 @@ x = (screen_width // 2) - (window_width // 2)
 y = (screen_height // 2) - (window_height // 2)
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-label1 = tk.Label(root, text="Привет!",font="Courier 20",bg="pink")
-label1.place(x=100, y=100)
-label2 = tk.Label(root, text="Введи дату своего рождения",font="Courier 15",bg="pink")
-label2.place(x=150, y=150)
-entry=tk.Entry(root,font=" 10")
-entry.place(x=150,y=180)
+label1 = tk.Label(root, text="Такого человека нет в нашей группе!",font="Courier 15",bg="red")
+label1.place(x=30, y=100)
+   
+def buttonback_click():
 
-def button1_click():
-    date=entry.get()
-    label3 = tk.Label(root, text="Вы",font="Courier 15",bg="pink")
-    label3.place(x=150, y=220)
-    buttonw = tk.Button(root, text="женщина",font="Courier 10",bg="pink",command=buttonw_click)
-    buttonw.place(x=210, y=220)
-    buttonm = tk.Button(root, text="мужчина",font="Courier 10",bg="blue",command=buttonm_click)
-    buttonm.place(x=290, y=220)
-    
-button1 = tk.Button(root, text="ok",font="Courier 10",command=button1_click)
-button1.place(x=355, y=180)
-
-
-def buttonw_click():
-    sex='ж'
     button_go = tk.Button(root, text="Рассчитать",font="Courier 15",command=go_click)
     button_go.place(x=100, y=266)
 
-    
-def buttonm_click():
-    sex='м'
-    button_go = tk.Button(root, text="Рассчитать",font="Courier 15",command=go_click)
-    button_go.place(x=100, y=266)
-
+buttonback = tk.Button(root, text="На главную",font="Courier 10",command=buttonback_click)
+buttonback.place(x=355, y=180)
 
 
 root.mainloop()    
