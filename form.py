1e6a52afddc6e078c611aca17a5a0ed10e8d3b13
LiliaSDyrnina <145978901+LiliaSDyrnina@users.noreply.github.com>
2023-10-21 17:47:52 +0300

    add

--- form.py
+++ form.py
@@ -7,8 +7,7 @@ root.title("Добро пожаловать в КНТ-6")
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-sex='9'
-date='qawsedrftgyhujikolp'
+
 
 window_width = 500
 window_height = 500
@@ -24,30 +23,125 @@ entry=tk.Entry(root,font=" 10")
 entry.place(x=150,y=180)
 
 def button1_click():
-    date=entry.get()
+    data=str(entry.get())
     label3 = tk.Label(root, text="Вы",font="Courier 15",bg="pink")
     label3.place(x=150, y=220)
     buttonw = tk.Button(root, text="женщина",font="Courier 10",bg="pink",command=buttonw_click)
     buttonw.place(x=210, y=220)
     buttonm = tk.Button(root, text="мужчина",font="Courier 10",bg="blue",command=buttonm_click)
     buttonm.place(x=290, y=220)
+    return data
     
 button1 = tk.Button(root, text="ok",font="Courier 10",command=button1_click)
 button1.place(x=355, y=180)
-def go_click():
-    root.destroy()
-    import bug
-
 def buttonw_click():
     sex='ж'
+    k=0
+    def go_click():
+        parameters=open('info.txt').readlines()
+        dates=[]        
+    
+        data=(entry.get())
+        print(sex)
+        sexes=[]
+        names=[]
+        signs=[]
+        captions=[]
+        connections=[]
+        for i in range(0,len(parameters)):
+            if i%6==0:
+                dates+=[''.join(parameters[i][0:-1:])]
+            if i%6==1:
+                sexes+=[''.join(parameters[i][0:-1:])]
+            if i%6==2:
+                names+=[''.join(parameters[i][0:-1:])]
+            if i%6==3:
+                signs+=[''.join(parameters[i][0:-1:])]
+            if i%6==4:
+                captions+=[''.join(parameters[i][0:-1:])]
+            if i%6==5:
+                connections+=[''.join(parameters[i][0:-1:])]
+        if data in dates: 
+            if sex=='ж':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+                        break
+            if sex=='м':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+            root.destroy()
+            import good
+            label1=''.join(('Привет, ',names[index],'!'))
+            label2=''.join(('Твой знак зодиака: ', signs[index]))
+            label3=''.join(('Твои личностные качества: ', captions[index]))
+            label4=''.join(('В нашей группе у тебя ты cможешь построить доверительные отношения с ', connections[index]))
+        else:
+            root.destroy()
+            import bug
+
+
     button_go = tk.Button(root, text="Рассчитать",font="Courier 15",command=go_click)
     button_go.place(x=100, y=266)
+    return k
 
     
 def buttonm_click():
     sex='м'
+    
+    def go_click():
+        parameters=open('info.txt').readlines()
+        dates=[]  
+    
+        data=(entry.get())
+        print(sex)
+        sexes=[]
+        names=[]
+        signs=[]
+        captions=[]
+        connections=[]
+        for i in range(0,len(parameters)):
+            if i%6==0:
+                dates+=[''.join(parameters[i][0:-1:])]
+            if i%6==1:
+                sexes+=[''.join(parameters[i][0:-1:])]
+            if i%6==2:
+                names+=[''.join(parameters[i][0:-1:])]
+            if i%6==3:
+                signs+=[''.join(parameters[i][0:-1:])]
+            if i%6==4:
+                captions+=[''.join(parameters[i][0:-1:])]
+            if i%6==5:
+                connections+=[''.join(parameters[i][0:-1:])]
+        print(dates)
+        if data in dates: 
+            if sex=='ж':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+                        break
+            if sex=='м':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+            root.destroy()
+            import good
+            label1=''.join(('Привет, ',names[index],'!'))
+            label2=''.join(('Твой знак зодиака: ', signs[index]))
+            label3=''.join(('Твои личностные качества: ', captions[index]))
+            label4=''.join(('В нашей группе у тебя ты cможешь построить доверительные отношения с ', connections[index]))
+        else:
+            root.destroy()
+            import bug
+
+
     button_go = tk.Button(root, text="Рассчитать",font="Courier 15",command=go_click)
     button_go.place(x=100, y=266)
+    
+
+
+
 
 
 
---
+++ good.py
@@ -0,0 +1,1 @@
+print("slgj;fklzjgkls")
